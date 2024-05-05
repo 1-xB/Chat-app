cebc4ccf13b662c18ee46fceed991adf2881097a
xB0bon <pan.bobon@gmail.com>
2024-05-05 15:58:06 +0200

    .

---
+++ voiceshare.py
@@ -0,0 +1,21 @@
+import time
+from vidstream import *
+import threading
+import sys
+
+sending = AudioSender('192.168.0.139', 9999)
+receive = AudioReceiver('192.168.0.131', 9999)
+
+t1 = threading.Thread(target=receive.start_server())
+t1.start()
+
+time.sleep(2)
+
+t2 = threading.Thread(target=sending.start_stream())
+t2.start()
+
+while input("") != 'stop':
+    continue
+
+receive.stop_server()
+sending.stop_stream()
